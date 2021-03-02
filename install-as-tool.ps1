3de7e935fdbf0d1a4c7ed98c36570d3c91970b07
carlossanlop <carlossanlop@users.noreply.github.com>
2021-03-02 10:26:58 -0800

    Fix install-as-tool script

--- install-as-tool.ps1
+++ install-as-tool.ps1
@@ -8,4 +8,4 @@ $PROJECT_NAME = "Program"
 $BUILD_CONFIGURATION = "Release"
 
 dotnet pack -c $BUILD_CONFIGURATION -o $ARTIFACTS_DIR $PROJECT_NAME
-dotnet tool update --global --add-source $ARTIFACTS_DIR $PROJECT_NAME
+dotnet tool update --global --add-source $ARTIFACTS_DIR "DocsPortingTool"

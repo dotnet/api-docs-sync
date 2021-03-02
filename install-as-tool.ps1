11f5167cf3a1780120c06dfb1699340a0ed9605d
carlossanlop <carlossanlop@users.noreply.github.com>
2021-03-02 10:32:20 -0800

    No need for

--- install-as-tool.ps1
+++ install-as-tool.ps1
@@ -4,8 +4,7 @@ $ErrorActionPreference = "Stop"
 Push-Location $(Split-Path $MyInvocation.MyCommand.Path)
 
 $ARTIFACTS_DIR = "artifacts"
-$PROJECT_NAME = "Program"
 $BUILD_CONFIGURATION = "Release"
 
-dotnet pack -c $BUILD_CONFIGURATION -o $ARTIFACTS_DIR $PROJECT_NAME
+dotnet pack -c $BUILD_CONFIGURATION -o $ARTIFACTS_DIR "Program"
 dotnet tool update --global --add-source $ARTIFACTS_DIR "DocsPortingTool"

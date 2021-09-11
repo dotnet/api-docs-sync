1efa62d88a8a2a524ddac84a28543e8aff7eba2b
Carlos Sanchez Lopez <1175054+carlossanlop@users.noreply.github.com>
2021-09-11 14:57:04 -0700

    Prevent uninstalling an existing tool if build fails

--- install-as-tool.ps1
+++ install-as-tool.ps1
@@ -8,6 +8,16 @@ $BUILD_CONFIGURATION = "Release"
 $APP_NAME = "DocsPortingTool"
 $EXE_PROJECT = "Program"
 
+Write-Output "Building $APP_NAME..."
+dotnet pack -c $BUILD_CONFIGURATION -o $ARTIFACTS_DIR $EXE_PROJECT
+
+# Prevent uninstalling an existing tool below if the build fails
+If ($LASTEXITCODE -ne 0)
+{
+	Write-Output "$APP_NAME will not be installed/upgraded."
+	Exit
+}
+
 $toolList = dotnet tool list --global
 # Output example when there are tools installed:
 
@@ -33,9 +43,6 @@ If ($toolList.Length -gt 2)
 		}
 	}
 }
-
-Write-Output "Building $APP_NAME..."
-dotnet pack -c $BUILD_CONFIGURATION -o $ARTIFACTS_DIR $EXE_PROJECT
 
 Write-Output "Installing $APP_NAME as a global tool..."
 dotnet tool install --global --add-source $ARTIFACTS_DIR $APP_NAME

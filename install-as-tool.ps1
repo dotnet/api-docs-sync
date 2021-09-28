942320ee0d2a13b50d392960e766a5bdd87ab0d7
Carlos Sanchez <1175054+carlossanlop@users.noreply.github.com>
2021-09-28 09:49:46 -0700

    Update install-as-tool.ps1
    Co-authored-by: Eirik Tsarpalis <eirik.tsarpalis@gmail.com>

--- install-as-tool.ps1
+++ install-as-tool.ps1
@@ -8,41 +8,13 @@ $BUILD_CONFIGURATION = "Release"
 $APP_NAME = "DocsPortingTool"
 $EXE_PROJECT = "Program"
 
-Write-Output "Building $APP_NAME..."
+dotnet clean -c $BUILD_CONFIGURATION; remove-item -Recurse -ErrorAction Ignore $ARTIFACTS_DIR
 dotnet pack -c $BUILD_CONFIGURATION -o $ARTIFACTS_DIR $EXE_PROJECT
 
-# Prevent uninstalling an existing tool below if the build fails
 If ($LASTEXITCODE -ne 0)
 {
 	Write-Output "$APP_NAME will not be installed/upgraded."
 	Exit
 }
 
-$toolList = dotnet tool list --global
-# Output example when there are tools installed:
-
-#   Package Id                 Version                 Commands
-#   ------------------------------------------------------------------
-#   docsportingtool            3.0.0                   DocsPortingTool
-#   microsoft.dotnet.darc      1.1.0-beta.21458.1      darc
-
-# When there are no tools installed, the output only contains:
-
-#   Package Id                 Version                 Commands
-#   ------------------------------------------------------------------
-
-If ($toolList.Length -gt 2)
-{
-	For ($i = 2; $i -lt $toolList.Length; $i++)
-	{
-		If ($toolList[$i].ToLowerInvariant().Contains($APP_NAME.ToLowerInvariant()))
-		{
-			Write-Output "Uninstalling existing $APP_NAME instance..."
-			dotnet tool uninstall --global $APP_NAME
-			Break;
-		}
-	}
-}
-
-Write-Output "Installing $APP_NAME as a global tool..."
-dotnet tool install --global --add-source $ARTIFACTS_DIR $APP_NAME
+dotnet tool update --global --add-source $ARTIFACTS_DIR $APP_NAME

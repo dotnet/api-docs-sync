45dbc5a84cc22b6635dc2cf9ba8c6adafbcf1d3b
carlossanlop <carlossanlop@users.noreply.github.com>
2022-07-15 14:35:57 -0700

    Fix naming and var violations, use Length instead of Count, remove unnecessary pragma ignore (bug fixed in roslyn).

--- src/PortToTripleSlash/src/libraries/ToTripleSlashPorter.cs
+++ src/PortToTripleSlash/src/libraries/ToTripleSlashPorter.cs
@@ -49,29 +49,26 @@ namespace ApiDocsSync.Libraries
             }
         }
 
-        private readonly Configuration Config;
-        private readonly DocsCommentsContainer DocsComments;
+        private readonly Configuration _config;
+        private readonly DocsCommentsContainer _docsComments;
 
-#pragma warning disable RS1024 // Compare symbols correctly
-        // Bug fixed https://github.com/dotnet/roslyn-analyzers/pull/4571
-        private readonly Dictionary<string, LocationInformation> ResolvedLocations = new();
-#pragma warning restore RS1024 // Compare symbols correctly
+        private readonly Dictionary<string, LocationInformation> _resolvedLocations = new();
 
-        private const string _allowedWarningMessage = "Found project reference without a matching metadata reference";
-        private static readonly string _pathSrcCoreclr = Path.Combine("src", "coreclr");
+        private const string AllowedWarningMessage = "Found project reference without a matching metadata reference";
+        private static readonly string s_pathSrcCoreclr = Path.Combine("src", "coreclr");
 
-        private static readonly string SystemPrivateCoreLib = "SYSTEM.PRIVATE.CORELIB";
+        private static readonly string s_systemPrivateCoreLib = "SYSTEM.PRIVATE.CORELIB";
 
-        private static readonly Dictionary<string, string> WorkspaceProperties = new() { { "RuntimeFlavor", "Mono" } };
+        private static readonly Dictionary<string, string> s_workspaceProperties = new() { { "RuntimeFlavor", "Mono" } };
 
-        private static readonly Dictionary<string, MSBuildWorkspace> Workspaces = new();
-        private static readonly Dictionary<string, MSBuildWorkspace> WorkspacesMono = new();
+        private static readonly Dictionary<string, MSBuildWorkspace> s_workspaces = new();
+        private static readonly Dictionary<string, MSBuildWorkspace> s_workspacesMono = new();
 
-        private static readonly Dictionary<string, Project> Projects = new();
-        private static readonly Dictionary<string, Compilation> Compilations = new();
+        private static readonly Dictionary<string, Project> s_projects = new();
+        private static readonly Dictionary<string, Compilation> s_compilations = new();
 
-        private static readonly Dictionary<string, Project> ProjectsMono = new();
-        private static readonly Dictionary<string, Compilation> CompilationsMono = new();
+        private static readonly Dictionary<string, Project> s_projectsMono = new();
+        private static readonly Dictionary<string, Compilation> s_compilationsMono = new();
 
         // If enabled via CLI arguments, a binlog file will be generated when running this tool.
         BinaryLogger? _binLogger = null;
@@ -79,14 +76,14 @@ namespace ApiDocsSync.Libraries
         {
             get
             {
-                if (Config.BinLogger)
+                if (_config.BinLogger)
                 {
                     if (_binLogger == null)
                     {
                         Log.Info("Enabling the collection of a binlog file...");
                         _binLogger = new BinaryLogger()
                         {
-                            Parameters = Path.Combine(Environment.CurrentDirectory, Config.BinLogPath),
+                            Parameters = Path.Combine(Environment.CurrentDirectory, _config.BinLogPath),
                             Verbosity = Microsoft.Build.Framework.LoggerVerbosity.Diagnostic,
                             CollectProjectImports = BinaryLogger.ProjectImportsCollectionMode.Embed
                         };
@@ -100,8 +97,8 @@ namespace ApiDocsSync.Libraries
         // Initializes a new porter instance with the specified configuration.
         private ToTripleSlashPorter(Configuration config)
         {
-            Config = config;
-            DocsComments = new DocsCommentsContainer(config);
+            _config = config;
+            _docsComments = new DocsCommentsContainer(config);
         }
 
         /// <summary>
@@ -120,15 +117,15 @@ namespace ApiDocsSync.Libraries
             // IMPORTANT: Need to load the MSBuild property BEFORE calling the ToTripleSlashPorter constructor.
             LoadVSInstance();
 
-            var porter = new ToTripleSlashPorter(config);
+            ToTripleSlashPorter porter = new(config);
             porter.Port();
         }
 
         // Collects the Docs xml files, the source code files, and ports the xml comments to triple slash.
         private void Port()
         {
-            DocsComments.CollectFiles();
-            if (!DocsComments.Types.Any())
+            _docsComments.CollectFiles();
+            if (!_docsComments.Types.Any())
             {
                 Log.Error("No Docs Type APIs found. Is the Docs xml path correct? Exiting.");
                 Environment.Exit(0);
@@ -136,30 +133,30 @@ namespace ApiDocsSync.Libraries
             Log.Info("Reading source code projects...");
 
             // Load and store the main project
-            ProjectInformation mainProjectInfo = GetProjectInfo(Config.CsProj!.FullName, isMono: false);
+            ProjectInformation mainProjectInfo = GetProjectInfo(_config.CsProj!.FullName, isMono: false);
 
-            foreach (DocsType docsType in DocsComments.Types.Values)
+            foreach (DocsType docsType in _docsComments.Types.Values)
             {
                 // If the symbol is not found in the current compilation, nothing to do - It means the Docs
                 // for APIs from an unrelated namespace were loaded for this compilation's assembly
                 if (!TryGetNamedSymbol(mainProjectInfo.Compilation, docsType.TypeName, out INamedTypeSymbol? symbol))
                 {
-                    Log.Info($"Type symbol '{docsType.FullName}' not found in compilation for '{Config.CsProj.FullName}'.");
+                    Log.Info($"Type symbol '{docsType.FullName}' not found in compilation for '{_config.CsProj.FullName}'.");
                     continue;
                 }
 
                 // Make sure at least one syntax tree of this symbol can be found in the current project's compilation
                 if (!symbol.Locations.Any())
                 {
-                    throw new NullReferenceException($"The symbol for the type '{docsType.FullName}' had no locations in '{Config.CsProj.FullName}'.");
+                    throw new NullReferenceException($"The symbol for the type '{docsType.FullName}' had no locations in '{_config.CsProj.FullName}'.");
                 }
 
-                Log.Cyan($"Type symbol '{docsType.FullName}' found in compilation for '{Config.CsProj.FullName}'.");
+                Log.Cyan($"Type symbol '{docsType.FullName}' found in compilation for '{_config.CsProj.FullName}'.");
 
                 // Otherwise, port the exact same comments in each location
                 AddSymbolLocationsToResolvedLocations(mainProjectInfo, symbol, docsType);
 
-                Log.Info($"Also trying to find '{symbol.Name}' in the referenced projects of project '{Config.CsProj.FullName}'...");
+                Log.Info($"Also trying to find '{symbol.Name}' in the referenced projects of project '{_config.CsProj.FullName}'...");
                 FindSymbolInReferencedProjects(docsType, mainProjectInfo.Project.ProjectReferences);
             }
 
@@ -175,13 +172,13 @@ namespace ApiDocsSync.Libraries
                 if (IsLocationTreeInCompilationTrees(location, projectInfo.Compilation))
                 {
                     Log.Info($"Symbol '{symbol.Name}' found in location '{path}'.");
-                    var info = new LocationInformation(docsType, location, projectInfo.Compilation);
+                    LocationInformation info = new(docsType, location, projectInfo.Compilation);
                     AddToResolvedSymbols(info);
                 }
                 else
                 {
                     Log.Info(false, $"Symbol '{symbol.Name}' not found in locations of project '{path}'.");
-                    if (n < symbol.Locations.Count())
+                    if (n < symbol.Locations.Length)
                     {
                         Log.Info(true, " Trying the next location...");
                     }
@@ -203,14 +200,14 @@ namespace ApiDocsSync.Libraries
 
                 // Skip looking in projects whose namespace that were explicitly excluded or not explicitly included
                 // The only exception is System.Private.CoreLib, which we should always explore
-                if (projectNamespaceToUpper != SystemPrivateCoreLib)
+                if (projectNamespaceToUpper != s_systemPrivateCoreLib)
                 {
-                    if (Config.ExcludedNamespaces.Any(x => x.StartsWith(projectNamespace)))
+                    if (_config.ExcludedNamespaces.Any(x => x.StartsWith(projectNamespace)))
                     {
                         Log.Info($"Skipping project '{projectPath}' which was added to -ExcludedNamespaces.");
                         continue;
                     }
-                    else if (!Config.IncludedNamespaces.Any(x => x.StartsWith(projectNamespace)))
+                    else if (!_config.IncludedNamespaces.Any(x => x.StartsWith(projectNamespace)))
                     {
                         Log.Info($"Skipping project '{projectPath}' which was not added to -IncludedNamespaces.");
                         continue;
@@ -228,8 +225,8 @@ namespace ApiDocsSync.Libraries
                     string monoProjectPath = Regex.Replace(projectPath, @"src(?<separator>[\\\/]{1})coreclr", "src${separator}mono");
 
                     // If the symbol was found in corelib, try to also find it in mono
-                    if (projectNamespaceToUpper == SystemPrivateCoreLib &&
-                        projectPath.Contains(_pathSrcCoreclr) &&
+                    if (projectNamespaceToUpper == s_systemPrivateCoreLib &&
+                        projectPath.Contains(s_pathSrcCoreclr) &&
                         TryFindSymbolInReferencedProject(monoProjectPath, docsType.TypeName, isMono: true, out ProjectInformation? monoProjectInfo, out INamedTypeSymbol? monoSymbol))
                     {
                         Log.Info($"Symbol '{monoSymbol.Name}' was also found in Mono locations of project '{monoProjectInfo.Project.FilePath}'.");
@@ -259,7 +256,7 @@ namespace ApiDocsSync.Libraries
         private void AddToResolvedSymbols(LocationInformation info)
         {
             string key = info.Tree.FilePath; // This ensures we have a unique key for symbols that are also found in mono
-            if (!ResolvedLocations.TryAdd(key, info))
+            if (!_resolvedLocations.TryAdd(key, info))
             {
                 Log.Info($"Skipping symbol tree already added for '{key}'.");
             }
@@ -287,10 +284,10 @@ namespace ApiDocsSync.Libraries
         private void PortDocsForResolvedSymbols()
         {
             Log.Info("Porting comments from Docs to triple slash...");
-            foreach ((string filePath, LocationInformation info) in ResolvedLocations)
+            foreach ((string filePath, LocationInformation info) in _resolvedLocations)
             {
                 Log.Info($"Porting docs for '{filePath}'...");
-                TripleSlashSyntaxRewriter rewriter = new(DocsComments, info.Model);
+                TripleSlashSyntaxRewriter rewriter = new(_docsComments, info.Model);
                 SyntaxNode newRoot = rewriter.Visit(info.Tree.GetRoot())
                     ?? throw new NullReferenceException($"Returned null root node for {info.Api.FullName} in {info.Tree.FilePath}");
 
@@ -357,22 +354,22 @@ namespace ApiDocsSync.Libraries
         private ProjectInformation GetProjectInfo(string projectPath, bool isMono)
         {
             MSBuildWorkspace workspace = GetOrAddWorkspace(
-                isMono ? WorkspacesMono : Workspaces,
-                isMono ? WorkspaceProperties : null,
+                isMono ? s_workspacesMono : s_workspaces,
+                isMono ? s_workspaceProperties : null,
                 projectPath);
 
             Project project = GetOrAddProject(
                 workspace,
-                isMono ? ProjectsMono : Projects,
+                isMono ? s_projectsMono : s_projects,
                 projectPath);
 
             Compilation compilation = GetOrAddCompilation(
                 workspace,
                 project,
-                isMono ? CompilationsMono : Compilations,
+                isMono ? s_compilationsMono : s_compilations,
                 projectPath);
 
-            var pd = new ProjectInformation(project, compilation, isMono);
+            ProjectInformation pd = new(project, compilation, isMono);
 
             return pd;
         }
@@ -449,11 +446,11 @@ namespace ApiDocsSync.Libraries
             ImmutableList<WorkspaceDiagnostic> diagnostics = workspace.Diagnostics;
             if (diagnostics.Any())
             {
-                var allMsgs = new List<string>();
+                List<string> allMsgs = new();
 
-                foreach (var diagnostic in diagnostics)
+                foreach (WorkspaceDiagnostic diagnostic in diagnostics)
                 {
-                    if (!diagnostic.Message.Contains(_allowedWarningMessage))
+                    if (!diagnostic.Message.Contains(AllowedWarningMessage))
                     {
                         allMsgs.Add($"    {diagnostic.Kind} - {diagnostic.Message}");
                     }
@@ -486,11 +483,11 @@ namespace ApiDocsSync.Libraries
         // Loads the external VS instance using the correct MSBuild dependency, which differs from the one used by this process.
         public static VisualStudioInstance LoadVSInstance()
         {
-            var vsBuildInstances = MSBuildLocator.QueryVisualStudioInstances().ToArray();
+            VisualStudioInstance[] vsBuildInstances = MSBuildLocator.QueryVisualStudioInstances().ToArray();
 
-            // https://github.com/carlossanlop/ApiDocsSync/issues/69
+            // https://github.com/dotnet/api-docs-sync/issues/69
             // Prefer the latest stable instance if there is one
-            var instance = vsBuildInstances
+            VisualStudioInstance instance = vsBuildInstances
                 .Where(b => !b.MSBuildPath.Contains("-preview")).OrderByDescending(b => b.Version).FirstOrDefault() ??
                 vsBuildInstances.First();
 
@@ -512,17 +509,17 @@ namespace ApiDocsSync.Libraries
             {
                 lock (s_guard)
                 {
-                    if (s_namesToAssemblies.TryGetValue(assemblyName.FullName, out var cachedAssembly))
+                    if (s_namesToAssemblies.TryGetValue(assemblyName.FullName, out Assembly? cachedAssembly))
                     {
                         return cachedAssembly;
                     }
 
-                    var assembly = TryResolveAssemblyFromPaths(context, assemblyName, searchPath, s_pathsToAssemblies);
+                    Assembly? assembly = TryResolveAssemblyFromPaths(context, assemblyName, searchPath, s_pathsToAssemblies);
 
                     // Cache assembly
                     if (assembly != null)
                     {
-                        var name = assembly.FullName;
+                        string? name = assembly.FullName;
                         if (name is null)
                         {
                             throw new Exception($"Could not get name for assembly '{assembly}'");
@@ -540,7 +537,7 @@ namespace ApiDocsSync.Libraries
         // Tries to find and return the specified assembly by looking in all the known locations where it could be found.
         private static Assembly? TryResolveAssemblyFromPaths(AssemblyLoadContext context, AssemblyName assemblyName, string searchPath, Dictionary<string, Assembly>? knownAssemblyPaths = null)
         {
-            foreach (var cultureSubfolder in string.IsNullOrEmpty(assemblyName.CultureName)
+            foreach (string? cultureSubfolder in string.IsNullOrEmpty(assemblyName.CultureName)
                 // If no culture is specified, attempt to load directly from
                 // the known dependency paths.
                 ? new[] { string.Empty }
@@ -549,17 +546,17 @@ namespace ApiDocsSync.Libraries
                 // bare search directory if that fails.
                 : new[] { assemblyName.CultureName, string.Empty })
             {
-                foreach (var extension in new[] { "ni.dll", "ni.exe", "dll", "exe" })
+                foreach (string? extension in new[] { "ni.dll", "ni.exe", "dll", "exe" })
                 {
-                    var candidatePath = Path.Combine(searchPath, cultureSubfolder, $"{assemblyName.Name}.{extension}");
+                    string candidatePath = Path.Combine(searchPath, cultureSubfolder, $"{assemblyName.Name}.{extension}");
 
-                    var isAssemblyLoaded = knownAssemblyPaths?.ContainsKey(candidatePath) == true;
+                    bool isAssemblyLoaded = knownAssemblyPaths?.ContainsKey(candidatePath) == true;
                     if (isAssemblyLoaded || !File.Exists(candidatePath))
                     {
                         continue;
                     }
 
-                    var candidateAssemblyName = AssemblyLoadContext.GetAssemblyName(candidatePath);
+                    AssemblyName candidateAssemblyName = AssemblyLoadContext.GetAssemblyName(candidatePath);
                     if (candidateAssemblyName.Version < assemblyName.Version)
                     {
                         continue;
@@ -567,7 +564,7 @@ namespace ApiDocsSync.Libraries
 
                     try
                     {
-                        var assembly = context.LoadFromAssemblyPath(candidatePath);
+                        Assembly assembly = context.LoadFromAssemblyPath(candidatePath);
                         return assembly;
                     }
                     catch

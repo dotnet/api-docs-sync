5cf29db90e32c083eb7ea2dde13e2de604b2820f
carlossanlop <carlossanlop@users.noreply.github.com>
2022-11-10 15:54:33 -0800

    Add AllTypesVisitor class.

---
+++ src/PortToTripleSlash/src/libraries/AllTypesVisitor.cs
@@ -0,0 +1,27 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+
+namespace ApiDocsSync.Libraries
+{
+
+    internal class AllTypesVisitor : SymbolVisitor
+    {
+        public readonly List<ISymbol> AllTypesSymbols = new();
+        public override void VisitNamedType(INamedTypeSymbol symbol)
+        {
+            AllTypesSymbols.Add(symbol);
+            foreach (INamedTypeSymbol typeMember in symbol.GetTypeMembers())
+            {
+                Visit(typeMember);
+            }
+        }
+        public override void VisitNamespace(INamespaceSymbol symbol) => Parallel.ForEach(symbol.GetMembers(), s => s.Accept(this));
+        public override void VisitDynamicType(IDynamicTypeSymbol symbol) => AllTypesSymbols.Add(symbol);
+        public override void VisitFunctionPointerType(IFunctionPointerTypeSymbol symbol) => AllTypesSymbols.Add(symbol);
+        public override void VisitAlias(IAliasSymbol symbol) => AllTypesSymbols.Add(symbol);
+    }
+}

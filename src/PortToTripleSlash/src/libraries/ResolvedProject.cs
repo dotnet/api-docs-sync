9ff5b83e844667e0a0fe25b4909989e65a928240
carlossanlop <carlossanlop@users.noreply.github.com>
2022-11-10 15:54:16 -0800

    Move Resolved* types to their own files.

---
+++ src/PortToTripleSlash/src/libraries/ResolvedProject.cs
@@ -0,0 +1,22 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Microsoft.CodeAnalysis;
+
+namespace ApiDocsSync.Libraries
+{
+    public class ResolvedProject
+    {
+        public ResolvedWorkspace ResolvedWorkspace { get; private set; }
+        public Project Project { get; private set; }
+        public Compilation Compilation { get; private set; }
+        public string ProjectPath { get; private set; }
+        public ResolvedProject(ResolvedWorkspace resolvedWorkspace, string projectPath, Project project, Compilation compilation)
+        {
+            ResolvedWorkspace = resolvedWorkspace;
+            Project = project;
+            Compilation = compilation;
+            ProjectPath = projectPath;
+        }
+    }
+}

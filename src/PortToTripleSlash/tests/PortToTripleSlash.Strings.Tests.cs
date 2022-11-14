efc75a4b99396b931b2f870ce6642d30e096be25
carlossanlop <carlossanlop@users.noreply.github.com>
2022-11-14 14:02:28 -0800

    Add new test class to handle strings.

---
+++ src/PortToTripleSlash/tests/PortToTripleSlash.Strings.Tests.cs
@@ -0,0 +1,19 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using ApiDocsSync.Tests;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace ApiDocsSync.PortToTripleSlash.Tests
+{
+    public class PortToTripleSlash_Strings_Tests : BasePortTests
+    {
+        public PortToTripleSlash_Strings_Tests(ITestOutputHelper output) : base(output)
+        {
+        }
+    }
+}

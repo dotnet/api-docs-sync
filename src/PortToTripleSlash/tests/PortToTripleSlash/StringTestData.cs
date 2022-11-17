e0d2ae876606c31727bbc28d06ab074f86c25d62
carlossanlop <carlossanlop@users.noreply.github.com>
2022-11-16 18:05:21 -0800

    Add one simple passing string test.

---
+++ src/PortToTripleSlash/tests/PortToTripleSlash/StringTestData.cs
@@ -0,0 +1,21 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Xml.Linq;
+
+namespace ApiDocsSync.PortToTripleSlash.Tests;
+
+internal class StringTestData
+{
+    public StringTestData(string docFile, string originalCode, string expectedCode)
+    {
+        DocFile = docFile;
+        OriginalCode = originalCode;
+        ExpectedCode = expectedCode;
+        XDoc = XDocument.Parse(DocFile);
+    }
+    public string DocFile { get; }
+    public string OriginalCode { get; }
+    public string ExpectedCode { get; }
+    public XDocument XDoc { get; }
+}
